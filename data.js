d1fa7284b532c080131723cf5d47be889d67657a
Baibhab Mondal <vbmandal98@gmail.com>
2018-07-23 19:55:49 +0530

    cities imported and indexed to elasticsearch

--- data.js
+++ data.js
@@ -1,4 +1,5 @@
 const elasticsearch = require('elasticsearch')
+const cities = require('./cities.json')
 
 const client = new elasticsearch.Client({
     hosts: ['http://localhost:9200']
@@ -15,16 +16,23 @@ error => {
     }
 })
 
-client.index({
-  index: 'elasticsearch',
-  type: 'cities_list',
-  body: {
-    "key1": "content1"
-  }
-  }, (err, res, status) => {
-  if (err) {
-    console.log(err)
-  } else {
-    console.log(res)
-  }
+
+var bulk = []
+
+cities.forEach(city => {
+  bulk.push({
+    index: {
+      _index: 'elasticsearch',
+      _type: 'cities_list'
+    }
+  })
+  bulk.push(city)
 })
+
+client.bulk({ body: bulk }, function (err, response) {
+  if (err) {
+    console.log("Failed Bulk operation", err)
+  } else {
+    console.log("Successfully imported", cities.length);
+  }
+});
